bf9d80e0fd8d6b2f4c30a839d3483bb8c8c0c460
Fraser Adams <fraser.adams@blueyonder.co.uk>
2018-01-15 15:38:01 +0000

    Add Nvidia specific Dockerfile and launch script to follow the book's narrative

---
+++ 4-local-applications/3D-accelerated-applications/glxgears/glxgearsV1.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+################################################################################
+# This script uses the simple approach of sharing the host's X11 socket with
+# the container. This method requires the container and display to be on the
+# same host, but gives performance that is equivalent to running the application
+# directly (i.e. not in a container) on the host.
+# This script creates an additional .Xauthority file based on the user's but
+# with a wildcard hostname to avoid having to set the container's hostname.
+# This script uses the -u option of docker run to reduce the priviledges of the
+# container to that of the user running the script, bind mounting /etc/passwd
+# read only isn't strictly necessary but allows the container to map the user's
+# ID to name to avoid seeing "I have no name!" when launching a shell.
+################################################################################
+
+if test -c "/dev/nvidia-modeset"; then
+    # Nvidia GPU
+    DOCKER_COMMAND=nvidia-docker
+    GPU_FLAGS="--device=/dev/nvidia-modeset -e LD_LIBRARY_PATH=/usr/local/nvidia/lib:/usr/local/nvidia/lib64:${LD_LIBRARY_PATH}"
+else
+    echo "This version of the glxgears launch script is Nvidia specific."
+    exit 1
+fi
+
+# If user isn't in docker group prefix docker with sudo 
+if ! (id -nG $(id -un) | grep -qw docker); then
+    DOCKER_COMMAND="sudo $DOCKER_COMMAND"
+fi
+
+# Create .Xauthority.docker file with wildcarded hostname.
+XAUTH=${XAUTHORITY:-$HOME/.Xauthority}
+DOCKER_XAUTHORITY=${XAUTH}.docker
+cp --preserve=all $XAUTH $DOCKER_XAUTHORITY
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $DOCKER_XAUTHORITY nmerge -
+
+$DOCKER_COMMAND run --rm \
+    -e DISPLAY=unix$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e XAUTHORITY=$DOCKER_XAUTHORITY \
+    -v $DOCKER_XAUTHORITY:$DOCKER_XAUTHORITY \
+    $GPU_FLAGS \
+    glxgears
+
