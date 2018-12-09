75b69e6b4fb92aee0d481b6da36b658d73c15450
Fraser Adams <fraser.adams@blueyonder.co.uk>
2018-12-09 20:48:32 +0000

    Add supertuxkart

---
+++ 5-more-complex-applications/games/supertuxkart/supertuxkart.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+BIN=$(cd $(dirname $0); echo ${PWD%docker-gui*})docker-gui/bin
+. $BIN/docker-xauth.sh
+. $BIN/docker-gpu.sh
+. $BIN/docker-pulseaudio.sh
+
+# Create a directory on the host that we can mount as a
+# "home directory" in the container for the current user. 
+mkdir -p $(id -un)/.config/pulse
+$DOCKER_COMMAND run --rm \
+    --device=/dev/input -v /dev/input:/dev/input:ro \
+    -u $(id -u):$(id -g) \
+    -v $PWD/$(id -un):/home/$(id -un) \
+    -v /etc/passwd:/etc/passwd:ro \
+    $PULSEAUDIO_FLAGS \
+    $X11_FLAGS \
+    $GPU_FLAGS \
+    supertuxkart $@
+
