c130d57f20f467d10cc2451ba3fd051a128e815f
Fraser Adams <fraser.adams@blueyonder.co.uk>
2019-01-19 08:39:57 +0000

    Add example decoupling x11vnc and xvfb from the applications, giving a much more modular microservice type pattern.

---
+++ 6-remote-applications/vnc/x11vnc-xvfb/x11vnc-xvfb.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+BIN=$(cd $(dirname $0); echo ${PWD%docker-gui*})docker-gui/bin
+. $BIN/docker-command.sh
+. $BIN/docker-pulseaudio-all.sh
+
+# Create a directory on the host that we can mount as a
+# "home directory" in the container for the current user. 
+mkdir -p $(id -un)/.config/pulse
+
+# Launch Xvfb and x11vnc exposing /tmp/.X11-unix as a volume.
+# Use -d option to daemonise and --init to run tini as pid 1
+$DOCKER_COMMAND run --rm -it -d \
+    --init \
+    --name=x11vnc-xvfb \
+    -p 5900:5900 \
+    -u $(id -u):$(id -g) \
+    -v $PWD/$(id -un):/home/$(id -un) \
+    -v /etc/passwd:/etc/passwd:ro \
+    x11vnc-xvfb
+
+# Launch firefox. Use --volumes-from to mount /tmp/.X11-unix
+# from x11vnc-xvfb and also use that container's IPC
+$DOCKER_COMMAND run --rm \
+    --shm-size 2g \
+    -u $(id -u):$(id -g) \
+    -v $PWD/$(id -un):/home/$(id -un) \
+    -v /etc/passwd:/etc/passwd:ro \
+    $PULSEAUDIO_FLAGS \
+    -e DISPLAY=:0 \
+    --ipc=container:x11vnc-xvfb \
+    --volumes-from x11vnc-xvfb \
+    firefox
+
+$DOCKER_COMMAND kill x11vnc-xvfb
