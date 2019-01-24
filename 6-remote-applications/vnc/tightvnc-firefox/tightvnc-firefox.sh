5e37faa4eeeaa597f6d01a788ac25da572f7b1d2
Fraser Adams <fraser.adams@blueyonder.co.uk>
2019-01-24 18:18:38 +0000

    Add example of tightvncserver and Firefox in same container

---
+++ 6-remote-applications/vnc/tightvnc-firefox/tightvnc-firefox.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+BIN=$(cd $(dirname $0); echo ${PWD%docker-gui*})docker-gui/bin
+. $BIN/docker-command.sh
+. $BIN/docker-pulseaudio-all.sh
+. $BIN/docker-dbus-all.sh
+
+# Create a directory on the host that we can mount as a
+# "home directory" in the container for the current user. 
+mkdir -p $(id -un)/.config/pulse
+mkdir -p $(id -un)/.config/dconf
+$DOCKER_COMMAND run --rm -it \
+    --init \
+    --shm-size 2g \
+    -p 5900:5900 \
+    -u $(id -u):$(id -g) \
+    -v $PWD/$(id -un):/home/$(id -un) \
+    -v /etc/passwd:/etc/passwd:ro \
+    $APPARMOR_FLAGS \
+    $DCONF_FLAGS \
+    $PULSEAUDIO_FLAGS \
+    -e DISPLAY=:1 \
+    -e GEOMETRY=1280x720 \
+    -e USER=$(id -un) \
+    tightvnc-firefox
+
+# Launch firefox. Use --volumes-from to mount /tmp/.X11-unix
+# from tightvnc and also use that container's IPC
+#$DOCKER_COMMAND run --rm \
+#    --shm-size 2g \
+#    -u $(id -u):$(id -g) \
+#    -v $PWD/$(id -un):/home/$(id -un) \
+#    -v /etc/passwd:/etc/passwd:ro \
+#    $APPARMOR_FLAGS \
+#    $DCONF_FLAGS \
+#    $PULSEAUDIO_FLAGS \
+#    -e DISPLAY=:1 \
+#    --ipc=container:tightvnc \
+#    --volumes-from tightvnc \
+#    firefox
+
+#$DOCKER_COMMAND kill tightvnc
