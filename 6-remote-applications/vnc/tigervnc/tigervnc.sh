d173e4004c883c7cf21c561e713bbc52b4f9fba5
Fraser Adams <fraser.adams@blueyonder.co.uk>
2019-09-01 18:45:57 +0100

    Change window manager from twm to jwm

--- 6-remote-applications/vnc/tigervnc/tigervnc.sh
+++ 6-remote-applications/vnc/tigervnc/tigervnc.sh
@@ -29,6 +29,12 @@ BIN=$(cd $(dirname $0); echo ${PWD%docker-gui*})docker-gui/bin
 mkdir -p $(id -un)/.config/pulse
 mkdir -p $(id -un)/.config/dconf
 
+# Create password if required.
+if ! test -f "$(id -un)/.vnc/passwd"; then
+    echo "creating password"
+    $(cd $(dirname $0); echo $PWD)/tigervnc-storepasswd.sh
+fi
+
 # Launch tigervncserver exposing /tmp/.X11-unix as a volume.
 # Use -d option to daemonise and --init to run tini as pid 1
 $DOCKER_COMMAND run --rm -it -d \
@@ -58,4 +64,4 @@ $DOCKER_COMMAND run --rm \
     --volumes-from tigervnc \
     firefox
 
-$DOCKER_COMMAND kill tigervnc
+$DOCKER_COMMAND stop tigervnc
