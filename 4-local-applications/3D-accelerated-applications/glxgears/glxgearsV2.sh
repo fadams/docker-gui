fede90378fb3309233ec71d63a69ade7a483ae1a
Fraser Adams <fraser.adams@blueyonder.co.uk>
2024-12-30 18:07:10 +0000

    Update Dockerfile to ubuntu:24.04

--- 4-local-applications/3D-accelerated-applications/glxgears/glxgearsV2.sh
+++ 4-local-applications/3D-accelerated-applications/glxgears/glxgearsV2.sh
@@ -36,23 +36,30 @@ DST=/usr/lib/x86_64-linux-gnu
 if test -c "/dev/nvidia-modeset"; then
     # Nvidia GPU
     if test -f "/usr/bin/nvidia-container-runtime"; then
-        # Nvidia Docker Version 2
+        # Nvidia Docker Version 2 or nvidia-container-toolkit
         # See https://github.com/NVIDIA/nvidia-container-runtime.
+        # https://github.com/NVIDIA/nvidia-container-toolkit
+        # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/docker-specialized.html#specialized-configurations-with-docker
 
         # Attempt to find the actual Nvidia library path. It should be
         # something like /usr/lib/nvidia-<driver version>
-        SRC=/usr/lib/x86_64-linux-gnu
-        if test -f "/etc/ld.so.conf.d/x86_64-linux-gnu_GL.conf"; then
-            SRC=$(cat /etc/ld.so.conf.d/x86_64-linux-gnu_GL.conf | grep /lib/)
-        fi
+        #SRC=/usr/lib/x86_64-linux-gnu
+        #if test -f "/etc/ld.so.conf.d/x86_64-linux-gnu_GL.conf"; then
+        #    SRC=$(cat /etc/ld.so.conf.d/x86_64-linux-gnu_GL.conf | grep /lib/)
+        #fi
 
         GPU_FLAGS="--runtime=nvidia "
         GPU_FLAGS+="--device=/dev/nvidia-modeset "
         GPU_FLAGS+="-e NVIDIA_VISIBLE_DEVICES=all "
-        GPU_FLAGS+="-e NVIDIA_DRIVER_CAPABILITIES=graphics "
-        GPU_FLAGS+="-v $SRC/libGL.so.1:$DST/libGL.so.1:ro "
-        GPU_FLAGS+="-v $SRC/libGLX.so.0:$DST/libGLX.so.0:ro "
-        GPU_FLAGS+="-v $SRC/libGLdispatch.so.0:$DST/libGLdispatch.so.0:ro "
+        GPU_FLAGS+="-e NVIDIA_DRIVER_CAPABILITIES=graphics,video "
+
+        # Previous version using Nvidia Docker Version 2 needed to mount
+        # libraries nvidia-container-toolkit doesn't seem to need that.
+        # Weirdly though that seems to need graphics,video not just graphics???
+        #GPU_FLAGS+="-e NVIDIA_DRIVER_CAPABILITIES=graphics "
+        #GPU_FLAGS+="-v $SRC/libGL.so.1:$DST/libGL.so.1:ro "
+        #GPU_FLAGS+="-v $SRC/libGLX.so.0:$DST/libGLX.so.0:ro "
+        #GPU_FLAGS+="-v $SRC/libGLdispatch.so.0:$DST/libGLdispatch.so.0:ro "
     else
         # Nvidia Docker Version 1
         echo "This script is specifically intended to illustrate nvidia-docker V2"
